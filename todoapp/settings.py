f84b5da7ce1b7a1ea773bf4efa0eb18ad79afa79
Lenovo <rybynok@mail.ru>
2019-07-19 20:55:10 +0300

    test

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "8=wn5u_6pps6w%4)6jygl=w*5p6s&522ilnu$^2(a_h+l@_xmt"
+SECRET_KEY = os.environ.get("SECRET_KEY", "not-so-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
