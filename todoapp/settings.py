8c8f54d9a9c77d73b50b4254110ea6a91740ebbc
Lenovo <rybynok@mail.ru>
2019-08-18 13:51:03 +0300

    finish

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -79,25 +79,27 @@ WSGI_APPLICATION = "todoapp.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "test_skilfactory",
+        'NAME': "test_skillfactory",
         'USER': "ovr1",
         'PASSWORD': 'G24O02d24230303',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+    }
+
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
-#AUTH_PASSWORD_VALIDATORS = [
-    #{
-    #    "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    #},
-    #{"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    #{"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    #{"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-#]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
 
 
 # Internationalization
