ee01a497107cac2a26b1e026015a788b8a08daa7
ovr1 <45287603+ovr1@users.noreply.github.com>
2019-08-18 18:13:07 +0300

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "todoapp.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "test_skillfactory",
+        'NAME': "postgres",
         'USER': "ovr1",
         'PASSWORD': 'G24O02d24230303',
         'HOST': '127.0.0.1',
